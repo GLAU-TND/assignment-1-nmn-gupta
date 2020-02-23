b720052bdcd11af35cae164818611da94898f4f2
naman <naman.gupta_cs18@gla.ac.in>
2020-02-23 16:21:52 +0530

    create constructor to initialize variables.

--- src/menu/linkedListMenu.java
+++ src/menu/linkedListMenu.java
@@ -1,13 +1,19 @@
 package menu;
 
 import adt.linkedListADT;
+import definition.linkedList;
 import person.Person;
 
 import java.util.Scanner;
 
 public class linkedListMenu<E> implements linkedListADT<E> {
-    private definition.linkedList<Person> linkedList;
+    private definition.linkedList<Person> myLinkedList;
     private Scanner sc;
+
+    public linkedListMenu() {
+        sc = new Scanner(System.in);
+        myLinkedList = new linkedList<>();
+    }
 
     @Override
     public void add() {
