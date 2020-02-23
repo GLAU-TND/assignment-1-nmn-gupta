c6129e2ea0a8d855ae6098ecb99215898b01caf9
naman <naman.gupta_cs18@gla.ac.in>
2020-02-23 15:37:28 +0530

    create sort() method to perform sorting.

--- src/definition/linkedList.java
+++ src/definition/linkedList.java
@@ -90,6 +90,23 @@ public class linkedList<T extends Comparable<T>> {
         return deletedNode;
     }
 
+    public void sort() {
+        Node<T> pass1 = getHead();
+        Node<T> pass2 = getHead().getNext();
+
+        while (pass1 != null) {
+            while (pass2 != null) {
+                if (pass1.getData().compareTo(pass2.getData()) > 0) {
+                    T temp = pass1.getData();
+                    pass1.setData(pass2.getData());
+                    pass2.setData(temp);
+                }
+                pass2 = pass2.getNext();
+            }
+            pass1 = pass1.getNext();
+        }
+    }
+
 
 }
 
