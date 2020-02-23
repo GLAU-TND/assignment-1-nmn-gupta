57f7cbba5b2c8425141d2c2e9bc44bd6fc7ee5d2
naman <naman.gupta_cs18@gla.ac.in>
2020-02-23 16:12:46 +0530

    implementing linkedListADT interface.

--- src/menu/linkedListMenu.java
+++ src/menu/linkedListMenu.java
@@ -1,4 +1,25 @@
 package menu;
 
-public class linkedListMenu<E> {
+import adt.linkedListADT;
+
+public class linkedListMenu<E> implements linkedListADT<E> {
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public E search() {
+        return null;
+    }
+
+    @Override
+    public void view() {
+
+    }
 }
