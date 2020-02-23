579f09ac761f32d160c520b48efe4e323a91c885
naman <naman.gupta_cs18@gla.ac.in>
2020-02-23 15:18:30 +0530

    importing Node class.

--- src/definition/linkedList.java
+++ src/definition/linkedList.java
@@ -1,4 +1,9 @@
 package definition;
 
+import node.Node;
+
 public class linkedList<T extends Comparable<T>> {
+    private Node<T> head;           //It is the beginning of linked list
+    private Node<T> iter;             //It is used to iterate the list
+
 }
