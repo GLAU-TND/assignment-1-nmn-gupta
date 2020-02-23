7e6cc4b94f775dae75298244c083f4cbfe3e13a0
naman <naman.gupta_cs18@gla.ac.in>
2020-02-23 17:01:58 +0530

    writing the menu.

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,13 @@ public class Main {
         linkedListMenu obj = new linkedListMenu();
         boolean flag = true;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Naman's Contact List App\n" +
+                "Press 1 to add a new contact\n" +
+                "Press 2 to view all contacts\n" +
+                "Press 3 to search for a contact\n" +
+                "Press 4 to delete a contact\n" +
+                "Press 5 to exit program");
+
 
 
     }
