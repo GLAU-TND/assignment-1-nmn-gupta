fbef0c1baecfa97f4cdf9fdf7e7061949ad7e41e
naman <naman.gupta_cs18@gla.ac.in>
2020-02-23 17:01:20 +0530

    creating Scanner class object.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,13 @@ package main;
 
 import menu.linkedListMenu;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         linkedListMenu obj = new linkedListMenu();
         boolean flag = true;
+        Scanner sc = new Scanner(System.in);
 
 
     }
