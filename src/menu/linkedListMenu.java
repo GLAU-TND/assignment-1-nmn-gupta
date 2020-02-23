970926b8f238cd437a4be47de6e3104be37071d0
naman <naman.gupta_cs18@gla.ac.in>
2020-02-23 16:19:12 +0530

    declaring variables.

--- src/menu/linkedListMenu.java
+++ src/menu/linkedListMenu.java
@@ -1,8 +1,14 @@
 package menu;
 
 import adt.linkedListADT;
+import person.Person;
+
+import java.util.Scanner;
 
 public class linkedListMenu<E> implements linkedListADT<E> {
+    private definition.linkedList<Person> linkedList;
+    private Scanner sc;
+
     @Override
     public void add() {
 
