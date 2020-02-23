f2e7ced1ae0897341650896b93eb354549bb5d94
naman <naman.gupta_cs18@gla.ac.in>
2020-02-23 15:11:29 +0530

    create getter and setter methods.

--- src/node/Node.java
+++ src/node/Node.java
@@ -4,4 +4,19 @@ public class Node<T> {
     private T data;   //  Data of node
     private Node<T> next;  //self referential data
 
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
 }
