5b7b9cf788d46a2ecf9ac3d70e647475da007a4b
naman <naman.gupta_cs18@gla.ac.in>
2020-02-23 15:42:04 +0530

    declaring variables.

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,6 +1,12 @@
 package person;
 
 public class Person implements Comparable<Person> {
+
+    private String firstName;                   //First name of person
+    private String lastName;                    //Last name of person
+    private String emailID;                     //EmailId of person
+    private StringBuffer phoneNumbers;          //Phone numbers of person
+
     @Override
     public int compareTo(Person o) {
         return 0;
