139bdb42cee843aae4318329120caee64247f20f
naman <naman.gupta_cs18@gla.ac.in>
2020-02-23 15:20:27 +0530

    create getter & setter methods.

--- src/definition/linkedList.java
+++ src/definition/linkedList.java
@@ -4,7 +4,22 @@ import node.Node;
 
 public class linkedList<T extends Comparable<T>> {
     private Node<T> head;           //It is the beginning of linked list
-    private Node<T> iter;             //It is used to iterate the list
+    private Node<T> iter;           //It is used to iterate the list
 
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public void setHead(Node<T> head) {
+        this.head = head;
+    }
+
+    public Node<T> getIter() {
+        return iter;
+    }
+
+    public void setIter(Node<T> iter) {
+        this.iter = iter;
+    }
 }
 
