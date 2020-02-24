03b4238a8c7ea07905a5509aa5b6e08a073840b5
naman <naman.gupta_cs18@gla.ac.in>
2020-02-24 17:43:09 +0530

    remove bug

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,7 @@ import menu.linkedListMenu;
 import java.util.Scanner;
 
 public class Main {
-    public static void 1
-
-    main(String[] args) {
+    public static void main(String[] args) {
         linkedListMenu obj = new linkedListMenu();
         boolean flag = true;
         Scanner sc = new Scanner(System.in);
